fe6afa858ecebc680bbe734d41fec4559693da7f
Alexis Buelvas Madera <alexjesus-4856@hotmail.com>
2024-06-06 02:49:01 -0500

    Implemeting filter on Checkbox component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -14,8 +14,9 @@ import shoppingCarIcon from "@/assets/icons/shoppingCarIcon.svg";
 import { StoreContext } from "@/provider/storeProvider";
 import { StoreContextProps } from "@/types/provider.interface";
 import AccordionComponent from "@/components/Accordion/Accordion";
+import ButtonComponent from "../Button/Button";
 const NavbarComponent = () => {
-  const { category, company, favorite, toggle } = useContext(
+  const { favorite, toggle, onCategory, onCompany, onReset } = useContext(
     StoreContext
   ) as StoreContextProps;
 
@@ -47,18 +48,62 @@ const NavbarComponent = () => {
         <NavbarItem className="hidden lg:flex"></NavbarItem>
       </NavbarContent>
       <NavbarMenu className="w-[20rem]">
-        <AccordionComponent key="1" title="Category">
-          {category.map((items) => (
-            <Checkbox className="flex flex-wrap w-[30rem]">{items}</Checkbox>
-          ))}
+        <AccordionComponent title="Category">
+          <Checkbox
+            onClick={() => onCategory("Classic Console")}
+            className="flex flex-wrap w-[11rem]"
+          >
+            Classic Console
+          </Checkbox>
+          <Checkbox
+            onClick={() => onCategory("Console")}
+            className="flex flex-wrap w-[7rem]"
+          >
+            Console
+          </Checkbox>
+          <Checkbox
+            onClick={() => onCategory("Classic Portable Console")}
+            className="flex flex-wrap w-[15rem]"
+          >
+            Classic Portable Console
+          </Checkbox>
+          <Checkbox
+            onClick={() => onCategory("Portable Console")}
+            className="flex flex-wrap w-[11rem]"
+          >
+            Portable Console
+          </Checkbox>
         </AccordionComponent>
         <Divider />
-        <AccordionComponent key="1" title="Company" className="text-greenPrimary">
-          {company.map((items) => (
-            <Checkbox className="flex flex-wrap w-[30rem]">{items}</Checkbox>
-          ))}
+        <AccordionComponent key="2" title="Company">
+          <Checkbox
+            onClick={() => onCompany("Sony")}
+            className="flex flex-wrap w-[10rem]"
+          >
+            Sony
+          </Checkbox>
+          <Checkbox
+            onClick={() => onCompany("Microsoft")}
+            className="flex flex-wrap w-[10rem]"
+          >
+            Microsoft
+          </Checkbox>
+          <Checkbox
+            onClick={() => onCompany("Nintendo")}
+            className="flex flex-wrap w-[10rem]"
+          >
+            Nintendo
+          </Checkbox>
         </AccordionComponent>
         <Divider />
+        <ButtonComponent
+          className="border-bluePrimary text-bluePrimary p-1 ml-2 w-[5rem]"
+          radius="sm"
+          variant="bordered"
+          onPress={onReset}
+        >
+          Reset
+        </ButtonComponent>
       </NavbarMenu>
     </Navbar>
   );
