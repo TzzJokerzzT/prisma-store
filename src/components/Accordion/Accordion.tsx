b7ac383358f432d3704fc0d6c67a600ec5e7a18e
Alexis Buelvas Madera <alexjesus-4856@hotmail.com>
2024-06-06 02:48:13 -0500

    Implementing filter

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,17 +1,13 @@
+import { AccordionProps } from "@/types/components.interface";
 import { Accordion, AccordionItem } from "@nextui-org/react";
 
-export interface AccordionPros {
-  children: React.ReactNode;
-  key: string | number;
-  title: string;
-  className?: string;
-}
+
 const AccordionComponent = ({
   children,
   key,
   title,
   className,
-}: AccordionPros) => {
+}: AccordionProps) => {
   return (
     <Accordion className="flex flex-wrap">
       <AccordionItem key={key} title={title} className={className}>
