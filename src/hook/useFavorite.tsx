aa1e27426fb6d139f33d49760a7d71aed37989e6
Alexis Buelvas Madera <alexjesus-4856@hotmail.com>
2024-06-10 19:25:02 -0500

    minor changes

--- src/hook/useFavorite.tsx
+++ src/hook/useFavorite.tsx
@@ -2,8 +2,9 @@ import { useCallback, useState } from "react";
 import { Products } from "@/types/provider.interface";
 
 const useFavorite = () => {
+  ///States
   const [favorite, setFavorite] = useState<Products[]>([]);
-  
+  ///Handler Function
   const handleFavorite = useCallback((item: Products) => {
     setFavorite((prev) => {
       const itemsExist = prev.find((items) => items.id === item.id);
