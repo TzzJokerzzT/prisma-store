78181485cb2e38918489a6b64ac0ce969151834d
Alexis Buelvas Madera <alexjesus-4856@hotmail.com>
2024-06-10 19:26:34 -0500

    minor changes

--- src/types/provider.interface.ts
+++ src/types/provider.interface.ts
@@ -1,3 +1,5 @@
+import React from "react";
+
 export interface StoreProviderProps {
   children: React.ReactNode;
 }
@@ -6,19 +8,28 @@ export interface StoreContextProps {
   isInView: boolean;
   setIsInView: React.Dispatch<React.SetStateAction<boolean>>;
   products: Products[];
-  category: string[];
-  company: string[];
   isActive: boolean;
   favorite: Products[];
   handleFavorite: (item: Products) => void;
   handleRemove: (item: Products) => void;
   isOpen: boolean;
   toggle: () => void;
-  selectedCategory: string;
-  selectedCompany: string;
-  onCategory: (items: string) => void;
-  onCompany: (items: string) => void;
-  onReset: () => void;
+  category: string;
+  setCategory: React.Dispatch<React.SetStateAction<string>>;
+  company: string;
+  setCompany: React.Dispatch<React.SetStateAction<string>>;
+  statusError: string;
+  showError: boolean;
+  setError: React.Dispatch<
+    React.SetStateAction<{
+      status: string;
+      show: boolean;
+    }>
+  >;
+  isSelectedCategory: number | null;
+  setIsSelectedCategory: React.Dispatch<React.SetStateAction<number | null>>;
+  isSelectedCompany: number | null;
+  setIsSelectedCompany: React.Dispatch<React.SetStateAction<number | null>>;
 }
 
 export type Products = {
