388263a6c1b8dc1461aa4c588e773cdee2a9b9cc
Alexis Buelvas Madera <alexjesus-4856@hotmail.com>
2024-06-10 19:25:45 -0500

    Implement error component

--- src/hook/useSidebarCar.tsx
+++ src/hook/useSidebarCar.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 
 const useSidebarCar = () => {
+  ///States
   const [isOpen, setIsOpen] = useState<boolean>(false);
-
+  ///Handler Functions
   const toggle = () => {
     setIsOpen((show) => !show);
   };
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,19 +1,27 @@
-import { Suspense, lazy } from "react";
+import { Suspense, lazy, useContext } from "react";
 import NavbarComponent from "@/components/Navbar/Navbar";
 import CarouselComponent from "@/components/Carousel/Carousel";
 import SpinnerComponent from "@/components/Loading/Loading";
 import SidebarCartComponent from "@/components/Sidebar/SidebarCart";
 import FooterComponent from "@/components/Footer/Footers";
+import { StoreContext } from "@/provider/storeProvider";
+import { StoreContextProps } from "@/types/provider.interface";
+import ErrorComponent from "@/components/Error/Error";
 const Card = lazy(() => import("@/components/Card/Card"));
 const Main = () => {
+  const { showError } = useContext(StoreContext) as StoreContextProps;
   return (
     <>
       <NavbarComponent />
       <SidebarCartComponent />
       <CarouselComponent />
-      <Suspense fallback={<SpinnerComponent />}>
-        <Card />
-      </Suspense>
+      {showError ? (
+        <ErrorComponent />
+      ) : (
+        <Suspense fallback={<SpinnerComponent />}>
+          <Card />
+        </Suspense>
+      )}
       <FooterComponent />
     </>
   );
