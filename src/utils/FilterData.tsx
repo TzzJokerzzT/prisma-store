38b109142e0eac874f8f6ba33e60ef39037b764b
Alexis Buelvas Madera <alexjesus-4856@hotmail.com>
2024-06-10 19:26:48 -0500

    Add filter data

---
+++ src/utils/FilterData.tsx
@@ -0,0 +1,9 @@
+export const categoryData: string[] = [
+  "Classic Console",
+  "Console",
+  "Classic Portable Console",
+  "Portable Console",
+];
+
+export const companyData: string[] = ["Sony", "Microsoft", "Nintendo"];
+
