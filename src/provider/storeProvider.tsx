25111125791df25e1f384c7db9ec8f78a69d274a
Alexis Buelvas Madera <alexjesus-4856@hotmail.com>
2024-06-10 19:25:58 -0500

    minor changes

--- src/provider/storeProvider.tsx
+++ src/provider/storeProvider.tsx
@@ -10,17 +10,25 @@ import useSidebarCar from "@/hook/useSidebarCar";
 export const StoreContext = createContext<StoreContextProps | null>(null);
 
 export const StoreProvider = ({ children }: StoreProviderProps) => {
-  const [isInView, setIsInView] = useState(false);
+  ///States
+  const [isInView, setIsInView] = useState<boolean>(false);
+  const [isSelectedCategory, setIsSelectedCategory] = useState<number | null>(
+    null
+  );
+  const [isSelectedCompany, setIsSelectedCompany] = useState<number | null>(
+    null
+  );
+  ///Custom Hooks Props
   const {
     products,
-    category,
-    company,
     isActive,
-    selectedCategory,
-    selectedCompany,
-    onCategory,
-    onCompany,
-    onReset,
+    category,
+    setCategory,
+    company,
+    setCompany,
+    statusError,
+    showError,
+    setError,
   } = useFetchData();
   const { favorite, handleFavorite, handleRemove } = useFavorite();
   const { isOpen, toggle } = useSidebarCar();
@@ -31,19 +39,23 @@ export const StoreProvider = ({ children }: StoreProviderProps) => {
         isInView,
         setIsInView,
         products,
-        category,
-        company,
         isActive,
         favorite,
         handleFavorite,
         handleRemove,
         isOpen,
         toggle,
-        selectedCategory,
-        selectedCompany,
-        onCategory,
-        onCompany,
-        onReset,
+        category,
+        setCategory,
+        company,
+        setCompany,
+        statusError,
+        showError,
+        setError,
+        isSelectedCategory,
+        setIsSelectedCategory,
+        isSelectedCompany,
+        setIsSelectedCompany,
       }}
     >
       {children}
